cdaad8ac7ff41e8f02ea509d14316ece65c6d6fd
Sergio Deleon Jr <sergio.electronicgenious9@gmail.com>
2022-07-18 17:47:31 -0500

    -Inital Commit

---
+++ Sources/App/routes.swift
@@ -0,0 +1,19 @@
+
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    let userController = UserController()
+    
+    // GET /users
+    router.get("users", use: userController.list) 
+    
+    // POST /users
+    router.post("users", use: userController.create)
+    
+    // PATCH /users/$id
+    router.patch("users", User.parameter, use: userController.update)
+    
+    // DELETE /users/$id
+    router.delete("users", User.parameter, use: userController.delete) 
+}
