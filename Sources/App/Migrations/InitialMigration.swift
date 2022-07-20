5ba5331378d65e88e591fd35d5e23e008e2e9e6c
Angelo <angelo@getohsome.com>
2022-07-20 04:10:38 +0200

    migration updated

--- Sources/App/Migrations/InitialMigration.swift
+++ Sources/App/Migrations/InitialMigration.swift
@@ -1,4 +1,6 @@
 import Fluent
+import FluentMongoDriver
+import MongoKitten
 
 struct InitialMigration: AsyncMigration {
     func prepare(on database: Database) async throws {
@@ -8,9 +10,24 @@ struct InitialMigration: AsyncMigration {
             .unique(on: "username", name: "no_duplicate_username")
             .field("password", .string, .required)
             .create()
+
+        // Index creation: a bit stupid its not build into fluent directly
+
+        // guard let db = (database as? MongoDatabaseRepresentable)?.raw else {
+        //     return
+        // }
+        // var keys = Document()
+        // keys["title"] = SortOrder.ascending.rawValue
+        // try await db["posts"]
+        //     .createIndex(named: "_title_", keys: keys)
+        //     .get()
     }
 
     func revert(on database: Database) async throws {
-        // Undo the change made in `prepare`, if possible.
+        try await database.schema("users")
+            .deleteUnique(on: "username")
+            .deleteField("username")
+            .deleteField("password")
+            .update()
     }
 }
