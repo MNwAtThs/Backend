bcc7189e1b94a246e5db79c3a98fa6bc67209f4b
Angelo <angelo@getohsome.com>
2022-07-20 22:51:25 +0200

    removed email requirement at login

--- Sources/App/Extensions/Dto+Vapor/LoginDto+Vapor.swift
+++ Sources/App/Extensions/Dto+Vapor/LoginDto+Vapor.swift
@@ -5,6 +5,7 @@ extension LoginDto.Response: Content {}
 
 extension LoginDto.Request: Validatable {
     public static func validations(_ validations: inout Validations) {
-        CreateUserDto.Request.validations(&validations)
+        validations.add("username", as: String.self, is: .count(4...), required: true)
+        validations.add("password", as: String.self, is: .count(6...), required: true)
     }
 }
